391e95d43233c9daac6b65fba3352da36b329738
laze <laze@G14D0S>
2018-02-28 00:22:23 +0100

    test de reprise en main

--- src/emalBundle/Controller/DefaultController.php
+++ src/emalBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace emalBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -10,5 +11,11 @@ class DefaultController extends Controller
     {
         // Attention la syntaxe généré par les commandes de symfony ne fonctionnent pas
         return $this->render('@emal/Default/index.html.twig');
+    }
+    
+    // test de reprise en main
+    public function helloAction($test="vide")
+    {
+        return new Response("texte ".$test);
     }
 }
