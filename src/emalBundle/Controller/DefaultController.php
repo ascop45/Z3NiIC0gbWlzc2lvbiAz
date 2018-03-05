33e6507b3d7481f207b1e7cc72c38762cb19d4c7
laze <laze@G14D0S>
2018-03-05 16:01:00 +0100

    controleur principal

--- src/emalBundle/Controller/DefaultController.php
+++ src/emalBundle/Controller/DefaultController.php
@@ -2,26 +2,46 @@
 
 namespace emalBundle\Controller;
 
+// controleur
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+
+// requete
+use Symfony\Component\HttpFoundation\Request;
+
+// formulaire
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        // Attention la syntaxe généré par les commandes de symfony ne fonctionnent pas
-        return $this->render('@emal/Default/index.html.twig');
-    }
-    
-    // test de reprise en main
-    public function helloAction()
-    {
-        return $this->redirect($this->generateUrl("emal_redirection"));
-    }
-    
-    
-    public function redirectionAction()
-    {
-        return new Response('redirection');
+        $request = Request::createFromGlobals();
+        
+        $form = $this->createFormBuilder()
+                ->add('identifiant', TextType::class)
+                ->add('motDePasse', PasswordType::class)
+                ->add('profil', ChoiceType::class,
+                        array('choices'=>array( 'visiteur'=>'Visiteur',
+                                                'comptable'=>'Comptable'))
+                )
+                ->add('valider', SubmitType::class)
+                ->getForm()
+        ;
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted())
+        {
+            $contenuFormulaire = $form->getData();
+            if($contenuFormulaire.profil === 'visiteur')
+            {   return $this->render('@emal/Defaut/visiteur/accueil_visiteur.html.twig');}
+            else
+            {   return $this->render('@emal/Defaut/comptable/accueil_comptable.html.twig'); }
+        }
+        
+        return $this->render('@emal/Default/index.html.twig', array('form'=>$form->createView()));
     }
 }
