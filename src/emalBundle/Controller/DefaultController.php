b728dc0fc4107835fbba1a6d38ed8acd829845d5
laze <laze@G14D0S>
2018-04-10 14:27:53 +0200

    mise à jour du contrôlleur par défaut : externalisation du formulaire

--- src/emalBundle/Controller/DefaultController.php
+++ src/emalBundle/Controller/DefaultController.php
@@ -7,76 +7,56 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 // requete
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 // formulaire
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use emalBundle\Form\ConnexionForm\ConnexionClass;
+use emalBundle\Form\ConnexionForm\ConnexionFormType;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // recuperation des variables transmis à la page
-        //$request = Request::createFromGlobals();
+        $request = Request::createFromGlobals();
         
-        // creation du formulaire procedural
-        $form = $this->createFormBuilder()
-                ->add('identifiant', TextType::class)
-                ->add('motDePasse', PasswordType::class)
-                ->add('profil', ChoiceType::class,
-                        array('choices'=>array( 'visiteur'=>'Visiteur',
-                                                'comptable'=>'Comptable'))
-                )
-                ->add('valider', SubmitType::class)
-                ->getForm()
-        ;
+        // Création du formulaire
+        $connexion = new ConnexionClass();
+        $form = $this->createForm(ConnexionFormType::class, $connexion);
+        
+        // Vérification de la complétion du formulaire
         $form->handleRequest($request);
-        
-        
-        if($form->isValid())
+        if($form->isSubmitted() && $form->isValid())
         {
-            $login = $form->get('identifiant')->getData();
+            $login = $form->get('login')->getData();
             $mdp = $form->get('motDePasse')->getData();
-            $profil = $form->get('profil')->getData();
+            $role = $form->get('role')->getData();
             
-            $repository = $this->getDoctrine()->getManager()->getRepository('emalBundle:'.$profil);
+            $repository = $this->getDoctrine()->getManager()->getRepository('emalBundle:'.ucfirst($role));
+            $donneeUtilisateur = $repository->findOneBy(array('login'=>$login, 'mdp'=>$mdp));
             
-            
-            if($profil === 'Visiteur')
-            {   return $this->render('@emal/visiteur/accueil_visiteur.html.twig', array('type'=> gettype($repository))); }
-            elseif($profi === 'Comptable')
-            {   return $this->render('@emal/comptable/accueil_comptable.html.twig'); }
+            if(isset($donneeUtilisateur))
+            {
+                return $this->redirectToRoute('accueil_'.$role, array('login'=>$login));
+            }
+            else
+            {
+                return $this->redirectToRoute('erreur', array('code'=>0));
+            }
         }
         
-        
-        // traitement si formulaire soumis
-        /*if($form->isValid())
-        {
-            if($form->get('profil')->getData() == 'Visiteur')
-            {
-                return $this->render('@emal/visiteur/accueil_visiteur.html.twig');
-            }
-            elseif($form->get('profil')->getData() == 'Comptable')
-            {
-                $id = $form->get('identifiant')->getData();
-                $mdp = hash("sha512", $form->get('motDePasse')->getData());
-                $retour = 'ok';
-                
-                $repository = $this->getDoctrine()->getManager()->getRepository('emalBundle:Comptable');
-                $visiteur = $repository->findOneBy(array('login'=>$id));
-                
-                if(!isset($visiteur) || $visiteur->getMdp() != $mdp)
-                { $retour = 'nope'; }
-                
-                return $this->render('@emal/comptable/accueil_comptable.html.twig', array('retour'=>$retour));
-            }
-        }*/
-        
-        // affichage du formulaire
+        // Affichage de la page de connexion
         return $this->render('@emal/Default/index.html.twig', array('form'=>$form->createView()));
+    }
+
+    
+    public function erreurAction(Request $request)
+    {
+        $messageErreur =    array(
+                                'Login, mot de passe ou rôle incorrect',
+                            );
+        
+        return $this->render(
+                '@emal/Default/erreur.html.twig',
+                array('message'=>$messageErreur[$request->attributes->get('code')])
+        );
     }
 }
